d954a9397c484cc5be3012f771feba352c1b9154
Benoit Serratrice <ben@chartreuse.local>
2020-06-22 21:08:15 +0800

    2121-06 - fix typo in ps1 script

--- labfiles/2121-06-CMP/getVmWithPs.ps1
+++ labfiles/2121-06-CMP/getVmWithPs.ps1
@@ -3,7 +3,7 @@ Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
 Connect-VIserver "vcsa-01a.corp.local" -username "administrator@regiona.local" -password "VMware1!" -ErrorAction Stop
 
 # Search for the VM by name
-$vmPartialName = $input.vmName
+$vmPartialName = $inputs.vmName
 Write-Host "Searching VM with name matching $vmPartialName"
 $matchingVms = Get-VM | Where-Object { $_.Name -like "*$vmPartialName*" }
 Write-Host "$($matchingVms.Count) VM(s) found with name matching $vmPartialName"
